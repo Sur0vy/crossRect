2541a6295bd2eb2de1bd812ca388549cd570ce5e
Sur0vy <Sur0vy@yandex.ru>
2014-09-18 12:12:19 +0400

    добавил обработку некорректного ввода

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class Main {
 
     //площади прямоугольников с учетом пересечений
-    public static int fullArea(RectangleList rl){
+    public static int fullArea(RectangleList rl) {
         if (rl.hasCrossed()) {
             //новые прямоугольники, образованные пересечением и вхождением
             RectangleList rlSub = new RectangleList(rl);
@@ -30,24 +30,37 @@ public class Main {
         //запустим цикл заполнения массива прямоугольников
         Scanner in = new Scanner(System.in);
         System.out.println("Enter rectangles count:");
-        int cnt = in.nextInt();
-        RectangleList rl = new RectangleList();
+        int cnt = 0;
+        try {
+            cnt = in.nextInt();
+        } catch (Exception e) {
+            System.out.println("Invalid rectangles count");
+        }
         if (cnt > 0) {
+            RectangleList rl = new RectangleList();
+            int x1;
+            int x2;
+            int y1;
+            int y2;
             for (int i = 0; i < cnt; i++) {
-                System.out.println("Enter \"" + (i + 1) + "\" rectangle coordinats:");
-                int x1 = in.nextInt();
-                int y1 = in.nextInt();
-                int x2 = in.nextInt();
-                int y2 = in.nextInt();
-                if ((x1 >= 0) && (x2 >= 0) && (y1 >= 0) && (y2 >= 0)) {
-                    rl.addRectangle(x1, y1, x2, y2);
-                } else {
-                    System.out.println("Invalid rectangle parameters!");
+                System.out.println("Enter \"" + (i + 1) + "\" rectangle coordinates:");
+                try {
+                    x1 = in.nextInt();
+                    y1 = in.nextInt();
+                    x2 = in.nextInt();
+                    y2 = in.nextInt();
+                    if ((x1 >= 0) && (x2 >= 0) && (y1 >= 0) && (y2 >= 0)) {
+                        rl.addRectangle(x1, y1, x2, y2);
+                    } else {
+                        System.out.println("Invalid rectangle parameters!");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid rectangle coordinates!");
                 }
             }
+            //делаем расчет
+            int rectArea = fullArea(rl);
+            System.out.println("Full area = " + rectArea);
         }
-        //делаем расчет
-        int rectArea = fullArea(rl);
-        System.out.println("Full area = " + rectArea);
     }
 }
--- src/com/company/RectangleCross.java
+++ src/com/company/RectangleCross.java
@@ -8,38 +8,38 @@ import java.awt.*;
  */
 public class RectangleCross extends Rectangle {
 
-    public RectangleCross(int x1, int y1, int x2, int y2){
-       if (x1 < x2){
-           x = x1;
-       } else {
-           x = x2;
-       };
-       width = Math.abs(x2 - x1);
-       if (y1 < y2){
-           y = y1;
-       } else {
-           y = y2;
-       };
-       height = Math.abs(y2 - y1);
-   }
-
-    public int area(){
-        return(width * height);
+    public RectangleCross(int x1, int y1, int x2, int y2) {
+        if (x1 < x2) {
+            x = x1;
+        } else {
+            x = x2;
+        }
+        width = Math.abs(x2 - x1);
+        if (y1 < y2) {
+            y = y1;
+        } else {
+            y = y2;
+        }
+        height = Math.abs(y2 - y1);
     }
 
-    public int getX1(){
+    public int area() {
+        return (width * height);
+    }
+
+    public int getX1() {
         return x;
     }
 
-    public int getX2(){
+    public int getX2() {
         return (x + width);
     }
 
-    public int getY1(){
+    public int getY1() {
         return y;
     }
 
-    public int getY2(){
+    public int getY2() {
         return (y + height);
     }
 
